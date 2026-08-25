dc729cd452b538b2755829ddcccf3b0f15f45518
kanato119 <kanatoro119@gmail.com>
2026-08-25 16:09:53 +0900

    SmokeScript

--- Assets/Scripts/Suzuki_Scripts/EffectManager.cs
+++ Assets/Scripts/Suzuki_Scripts/EffectManager.cs
@@ -2,13 +2,7 @@ using UnityEngine;
 
 public class EffectManager : MonoBehaviour
 {
-    /*
-    エフェクトが必要なプレイヤーを調べる 
-    プレイヤーの座標を調べて
-    なんのエフェクトが必要なのか調べる
-    エフェクトを流す関数を渡す
-
-     */
+  
     [SerializeField] public ParticleSystem Smokeparticle;
     [SerializeField] public ParticleSystem Healparticle;
 
