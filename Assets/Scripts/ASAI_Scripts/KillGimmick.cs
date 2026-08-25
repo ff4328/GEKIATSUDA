f6edb8a780bd8e4bd3ab1d287881d6b5475a5024
sakana619 <a.rihito619@icloud.com>
2026-08-25 17:22:38 +0900

    即死ギミック処理を追加

---
+++ Assets/Scripts/ASAI_Scripts/KillGimmick.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+/// <summary>
+/// 即死のダメージギミック
+/// </summary>
+public class KillGimmick : DamageGimmickBase
+{
+
+    public override void HitToCharacter(BaseCharacter hitCharacter)
+    {
+
+        //当たったキャラクターを即死させる
+
+        //hitCharacter.Dead();
+
+    }
+
+}
