7c2731be2f26bfa3ee831a2979dbfcd7e881f82e
Sakana619 <a.rihito619@icloud.com>
2026-08-25 12:04:08 +0900

    通り抜け可能な床の実装途中まで

--- Assets/Scripts/ASAI_Scripts/CanPassFloor.cs
+++ Assets/Scripts/ASAI_Scripts/CanPassFloor.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
@@ -5,13 +6,79 @@ using UnityEngine;
 /// </summary>
 public class CanPassFloor : StageGimmickBase
 {
+    //コリジョンを無効にする時間
+    private const float _COLLISION_IGNORE_TIME = 1.1f;
 
 
+    /// <summary>
+    /// 無効にしているコリジョンのデータ
+    /// </summary>
+    private struct IgnoreCollisionData
+    {
+        public IgnoreCollisionData(Collider collider)
+        {
+            this.collider = collider;
+            ignoreTimeSec = 0;
+        }
+
+        //無効にするコライダー
+        Collider collider;
+        //無効にしている時間
+        public float ignoreTimeSec;
+    }
+    //無効なコリジョンのデータリスト
+    private List<IgnoreCollisionData> _ignoreCollisionList;
+
+    protected override void Start()
+    {
+        base.Start();
+        _ignoreCollisionList = new List<IgnoreCollisionData>();
+    }
+
+    private void Update()
+    {
+        //空かチェック
+        if (_ignoreCollisionList.Count <= 0) return;
+
+        float deltaTime = Time.deltaTime;
+
+        for(int i = 0; i < _ignoreCollisionList.Count; i++)
+        {
+            IgnoreCollisionData checkData = _ignoreCollisionList[i];
+
+            //経過時間を加算
+            checkData.ignoreTimeSec += deltaTime;
+            //コリジョンを有効にするタイミングでなければ処理しない
+            if (checkData.ignoreTimeSec < _COLLISION_IGNORE_TIME) continue;
+
+
+        }
+
+
+    }
 
     public override void HitToCharacter(CharacterBase hitCharacter)
     {
+        if (hitCharacter == null || collider == null) return;
         //もし移動方向が下ならコリジョンを無効にする
+        if (!Input.GetKey(KeyCode.L)) return;
 
+        //無効にするコリジョンを取得
+        Collider[] ignoreCollisionList = hitCharacter.GetColliders();
+
+        for (int i = 0; i < ignoreCollisionList.Length; i++)
+        {
+            Collider ignoreCollider = ignoreCollisionList[i];
+            if (ignoreCollider == null) continue;
+
+            //コリジョンを無効にする
+            Physics.IgnoreCollision(ignoreCollider, collider, false);
+
+            //無効にするコリジョンリストのデータ作成
+            IgnoreCollisionData newData = new IgnoreCollisionData(ignoreCollider);
+            //無効にするコリジョンリストに追加
+            _ignoreCollisionList.Add(newData);
+        }
 
     }
 
--- Assets/Scripts/ASAI_Scripts/StageGimmickBase.cs
+++ Assets/Scripts/ASAI_Scripts/StageGimmickBase.cs
@@ -1,4 +1,3 @@
-using NUnit.Framework;
 using UnityEngine;
 
 /// <summary>
@@ -13,7 +12,7 @@ public abstract class StageGimmickBase : MonoBehaviour
     {
         if(collider == null)
         {
-            Assert.Fail("StageGimmickのコライダーをセットしていない");
+            Debug.Log("StageGimmickのコライダーをセットしていない");
             collider = gameObject.AddComponent<BoxCollider>();
         }
 
@@ -45,6 +44,15 @@ public abstract class StageGimmickBase : MonoBehaviour
     protected void SetActive(bool active)
     {
         gameObject.SetActive(active);
+    }
+
+    /// <summary>
+    /// コライダーの有効、無効を設定
+    /// </summary>
+    /// <param name="enable"></param>
+    protected void SetEnabledCollider(bool enable)
+    {
+        collider.enabled = enable;
     }
 
 }
