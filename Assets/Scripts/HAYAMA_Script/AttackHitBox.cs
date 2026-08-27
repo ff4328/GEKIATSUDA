254ff79cf085971e822ab48daac66090278b623b
bamboojr36 <towadeku0306@gmail.com>
2026-08-27 12:06:24 +0900

    攻撃範囲選択追加

--- Assets/Scripts/HAYAMA_Script/AttackHitBox.cs
+++ Assets/Scripts/HAYAMA_Script/AttackHitBox.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 public class AttackHitBox : MonoBehaviour
 {
-    public SwordMan owner;
     private Collider col;
     public bool isPowerUp = false;
     public BaseCharacter character;
