7dc42ee88ab6edb10f31c6cf9f8e89f2f302bfde
sakana619 <a.rihito619@icloud.com>
2026-08-28 11:25:07 +0900

    当たり判定の処理変更

--- Assets/Scripts/ASAI_Scripts/LavaGimmick.cs
+++ Assets/Scripts/ASAI_Scripts/LavaGimmick.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 /// </summary>
 public class LavaGimmick : DamageGimmickBase
 {
+    //下のマグマか
+    [SerializeField]
+    private bool _isUnder = true;
 
     protected override void Start()
     {
@@ -18,9 +21,20 @@ public class LavaGimmick : DamageGimmickBase
         Collider hitCollider = hitCharacter.GetComponent<Collider>();
         //最近接点を求める
         Vector3 closestPoint = collider.ClosestPoint(hitCollider.bounds.center);
-        hitCharacter.ApplyKnockback(damage, closestPoint);
 
-        Debug.Log("HIT");
+        //下からあたる前提
+
+        //めり込んでいたら
+        if (hitCharacter.transform.position.y < closestPoint.y && _isUnder)
+        {
+            //めり込み量を調べる
+            float overlapY = hitCharacter.transform.position.y - closestPoint.y;
+            //めり込み量を足す
+            closestPoint.y += overlapY;
+
+        }
+
+        hitCharacter.OnHit(damage, closestPoint);
 
     }
 
--- Assets/Scripts/ASAI_Scripts/NeedleGimmick.cs
+++ Assets/Scripts/ASAI_Scripts/NeedleGimmick.cs
@@ -18,13 +18,18 @@ public class NeedleGimmick : DamageGimmickBase
         Collider hitCollider = hitCharacter.GetComponent<Collider>();
         //最近接点を求める
         Vector3 closestPoint = collider.ClosestPoint(hitCollider.bounds.center);
-        Debug.Log(damage);
 
-        Vector3 direction = Vector3.zero;
-        float distance = 0;
+        //下からあたる前提
 
-        Physics.ComputePenetration(hitCollider, hitCollider.transform.position, hitCollider.transform.rotation,
-            collider, collider.transform.position, collider.transform.rotation, out direction, out distance);
+        //めり込んでいたら
+        if (hitCharacter.transform.position.y < closestPoint.y)
+        {
+            //めり込み量を調べる
+            float overlapY = hitCharacter.transform.position.y - closestPoint.y;
+            //めり込み量を足す
+            closestPoint.y += overlapY;
+
+        }
 
         hitCharacter.OnHit(damage, closestPoint);
 
